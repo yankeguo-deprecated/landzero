5dfcce6a9ac59f0ad1dccbefbfdc412930c04f9f
Guo Y.K <hi@guoyk.net>
2020-03-02 09:31:00 +0800

    update i18n

--- landzero/settings.py
+++ landzero/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'zh_hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -103,6 +103,16 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LANGUAGES = (
+    ('en', ('English')),
+    ('zh_hans', ('简体中文'))
+)
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
