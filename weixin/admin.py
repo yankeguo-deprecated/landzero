431f1c8246d99ebeaad27b4816f5934ada948bd3
Guo Y.K <hi@guoyk.net>
2020-03-08 21:53:03 +0800

    Update admin.py

--- weixin/admin.py
+++ weixin/admin.py
@@ -5,6 +5,7 @@ from .models import OpenId
 
 class OpenIdAdmin(admin.ModelAdmin):
     list_display = ['id', 'open_id', 'user']
+    raw_id_fields = ['user']
 
 
 admin.site.register(OpenId, OpenIdAdmin)
