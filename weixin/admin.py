e47aefcd5237c11434ff40fa1d9cbf817bf2f1d7
Guo Y.K <hi@guoyk.net>
2020-03-08 13:17:51 +0800

    register admin

--- weixin/admin.py
+++ weixin/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import OpenId
+
+
+class OpenIdAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(OpenId, OpenIdAdmin)
