41e55c9a1a4ab0136e05bd0604f231c34247bde3
Guo Y.K <hi@guoyk.net>
2020-03-30 14:59:25 +0800

    add Post

--- www/admin.py
+++ www/admin.py
@@ -1,1 +1,13 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'slug', 'timestamp']
+    raw_id_fields = ['user']
+    readonly_fields = ['content_html']
+
+
+admin.site.register(Post, PostAdmin)
--- www/models.py
+++ www/models.py
@@ -1,1 +1,23 @@
 # Create your models here.
+import markdown
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import pre_save
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    content_md = models.TextField()
+    content_html = models.TextField()
+
+
+def pre_save_post_receiver(sender, instance: Post, *args, **kwargs):
+    if not instance.content_md:
+        instance.content_md = ''
+    instance.content_html = markdown.markdown(instance.content_md)
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
