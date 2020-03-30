d7140b1ee5af55b48320a98b326ec4afe25bf7ba
Guo Y.K <hi@guoyk.net>
2020-03-30 15:19:34 +0800

    Update admin.py

--- www/admin.py
+++ www/admin.py
@@ -1,5 +1,6 @@
 # Register your models here.
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Post
 
@@ -7,7 +8,11 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'title', 'slug', 'timestamp']
     raw_id_fields = ['user']
-    readonly_fields = ['content_html']
+    readonly_fields = ['content_html_preview']
+    exclude = ['content_html']
+
+    def content_html_preview(self, obj):
+        return mark_safe(obj.content_html)
 
 
 admin.site.register(Post, PostAdmin)
